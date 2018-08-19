da0f8b9843f7028603e84b09ff36dc4ec2876b74
juliusnonnenmacher <juliusnonn@web.de>
2018-08-19 13:32:22 +0200

    Pull Alina, Grundgerüst eingebaut

--- app/src/main/java/wip/me/fhdw/de/tenmanager/Notes/InitNoteDetailView_Alina.java
+++ app/src/main/java/wip/me/fhdw/de/tenmanager/Notes/InitNoteDetailView_Alina.java
@@ -1,10 +1,15 @@
 package wip.me.fhdw.de.tenmanager.Notes;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import wip.me.fhdw.de.tenmanager.AppDatabase;
@@ -21,8 +26,9 @@ public class InitNoteDetailView_Alina extends AppCompatActivity{
     private ApplicationLogicNoteDetailView_Alina mApplicationLogic;
     private NoteData_Julius mData;
     private AppDatabase mDb;
-
     private Intent mIntent;
+    private ImageView imageView;
+
 
     //todo ApplicationLogic inizialisieren
 
@@ -35,8 +41,17 @@ public class InitNoteDetailView_Alina extends AppCompatActivity{
         initApplicationLogic(mIntent);
         //initDb();
 
-    }
+        ImageButton btnCamera = (ImageButton)findViewById(R.id.btnCamera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
 
+            }
+        });
+    }
 
     private void initGui(){
         mGui = new GuiNoteDetailView_Alina(this);
@@ -54,7 +69,7 @@ public class InitNoteDetailView_Alina extends AppCompatActivity{
 
     public void initToolbar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
-        //getSupportActionBar().hide();
+        getSupportActionBar().hide();
         TextView toolbarTextview = toolbar.findViewById(R.id.toolbar_textview);
         toolbarTextview.setText("Note");
     }
@@ -70,6 +85,8 @@ public class InitNoteDetailView_Alina extends AppCompatActivity{
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // super.onActivityResult(requestCode, resultCode, data);
         mApplicationLogic.onActivityReturned(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 
     @Override
